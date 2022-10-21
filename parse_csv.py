3d96819f9a828e8dafe612f2c06afa18ac999c7a
Masud Al Nahid <mdmsd2@gmail.com>
2022-10-22 03:14:39 +0600

    Add all my files

--- parse_csv.py
+++ parse_csv.py
@@ -1,18 +1,42 @@
 import csv
-
-html_output = ''
+import json
+from subprocess import list2cmdline
+html_output = []
 names = []
 
-#This 'my_list.csv' file have bunch line of data list 
+
+def pylist_to_list(list):
+    temp_list = ['<ul>']
+    for element in list:
+        temp_list.append(f'<li>{element}</li>')
+
+    temp_list.append('</ul>')
+    html_string = ''.join(temp_list)
+    return html_string
+
+
+# This 'my_list.csv' file have bunch line of data list
 with open('my_list.csv', 'r') as data_file:
     csv_data = csv.DictReader(data_file)
-    #Ingredients is header name
+    # Ingredients is header name
     for line in csv_data:
         if line['Ingredients'] == 'No Reward':
             break
         names.append(f"{line['Ingredients']}")
 
-for name in names:
-    html_output += f'\n\t <li>{name}</li>'
 
-print(html_output)
+for name in names:
+    name = name.replace(']', '').replace('[', '').replace('\'', '"')
+    ingredient_list = name.split('"')
+
+    while '' in ingredient_list:
+        ingredient_list.remove('')
+
+    while ', ' in ingredient_list:
+        ingredient_list.remove(', ')
+
+    single_element = pylist_to_list(ingredient_list)
+    html_output.append(single_element)
+
+
+print(html_output[0])
